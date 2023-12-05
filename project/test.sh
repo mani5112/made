212e858b4d88f482504c449adc39e624b928b3f2
Manikanta Varaprasad <42089393+mani5112@users.noreply.github.com>
2023-12-05 23:50:09 +0100

    Create test.sh
    updated test.sh

---
+++ project/test.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+# Install Python packages
+pip install opendatasets
+pip install pandas
+pip install pysqlite3
+pip install kaggle
+pip install logging
+
+
+python3 get_data.py
+python3 test.py
