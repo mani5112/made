051e1539869388ef02321a02eb59d17ee5724fdb
Manikanta Varaprasad <42089393+mani5112@users.noreply.github.com>
2023-12-20 12:15:27 +0100

    Update get_data.py

--- project/get_data.py
+++ project/get_data.py
@@ -11,7 +11,7 @@ dataset_links = [
 
 # have to provide kaggle json file to download the .csv files
 od.download(dataset_links[0])
-od.download(dataset_links[1])
+
 
 # storing the elon must tweets into a sqlite database
 path_elon_tweets = 'elon-musk-tweets/elonmusk_tweets.csv'
@@ -21,12 +21,13 @@ logging.info(f'storing the tweets data into a database under data folder ')
 conn = sqlite3.connect(tweet_db_path)
 df_tweets.to_sql('elon_tweets', conn, index=False, if_exists='replace')
 
+od.download(dataset_links[1])
+
 # storing the tesla tweets into a sqlite dataase
 path_tesla_stocks = 'tesla-stock-data-from-2010-to-2020/TSLA.csv'
-airbnb_df = pd.read_csv(path_tesla_stocks)
-tsla_stock_db_path = '../data/tsla_stocks.sqlite'
-conn = sqlite3.connect(tsla_stock_db_path)
+tsla_stock_df = pd.read_csv(path_tesla_stocks)
+conn = sqlite3.connect("../data/tsla_stocks.sqlite")
 logging.info(f'storing the stocks data into a database under data folder ')
-airbnb_df.to_sql('tsla_stocks', conn, index=False, if_exists='replace')
+tsla_stock_df.to_sql('tsla_stocks', conn, index=False, if_exists='replace')
 # closing the database connection
 conn.close()
